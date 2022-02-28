c88678ae865c434f70b79a20404882417e0dc2eb
shrey <shreyassshedge26@gmail.com>
2022-02-28 09:20:24 -0500

    1st commit

---
+++ Cap1/src/Day0/Hello.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package Day0;
+
+/**
+ * @author shrey
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello shreyas shedge its me");
+
+	}
+
+}
